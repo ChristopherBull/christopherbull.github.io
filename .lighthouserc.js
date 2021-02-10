39cb8525c98b8dfeeedfe2d7157a8f1e7ec89a0a
Christopher Bull <chrisbull88@gmail.com>
2021-02-10 12:54:42 +0000

    Chore: disable Lighthouse 'uses-http2' assertion

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -6,6 +6,10 @@ module.exports = {
     },
     assert: {
       preset: 'lighthouse:recommended',
+      assertions: {
+        // Cannot check http2 on local Http server through Lighthouse CI
+        'uses-http2': 'off',
+      },
     },
     upload: {
       target: 'temporary-public-storage',
