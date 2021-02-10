8eba1a1afedaf4c1b9d5a6ed2cc0e42fae4bea2e
Christopher Bull <chrisbull88@gmail.com>
2021-02-10 12:31:15 +0000

    Chore(ci): Add Lighthouse recommended assertions

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -4,6 +4,9 @@ module.exports = {
       url: ['http://localhost:4000/'],
       startServerCommand: 'bundle exec jekyll serve',
     },
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
