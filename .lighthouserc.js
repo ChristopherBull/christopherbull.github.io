fd93a970760a3abda3c2f13d246e9927c3c50d4b
Christopher Bull <chrisbull88@gmail.com>
2021-02-11 16:06:24 +0000

    Fix: Skip audits, GoogleChrome/lighthouse-ci#548

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,20 +1,15 @@
 module.exports = {
   ci: {
     collect: {
-      // Specify URLs to avoid LHCI reporting an error only seen in CI/localhost
-      // ('canonical==off' does not appear to prevent the error):
-      // "Document does not have a valid `rel=canonical` Relative URL (/)"
       url: ['http://localhost:4000/'],
       startServerCommand: 'bundle exec jekyll serve',
+      settings: {
+        // Cannot check these tests on local Http server through Lighthouse CI
+        skipAudits: ['canonical', 'uses-http2', 'uses-long-cache-ttl'],
+      },
     },
     assert: {
       preset: 'lighthouse:recommended',
-      assertions: {
-        // Cannot check these tests on local Http server through Lighthouse CI
-        'uses-http2': ['off', {}],
-        'uses-long-cache-ttl': ['off', {}],
-        canonical: ['off', {}],
-      },
     },
     upload: {
       target: 'temporary-public-storage',
