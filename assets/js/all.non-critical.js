3cc9cb5ab7a50cbbdf2ad56430669a6e608f4726
Christopher Bull <3485184+ChristopherBull@users.noreply.github.com>
2022-11-10 14:38:05 +0000

    Fix: Prepend rel="me" instead of replace

--- assets/js/all.non-critical.js
+++ assets/js/all.non-critical.js
@@ -6,8 +6,9 @@ $(function () {
         var host = document.location.host;
         var re = new RegExp(host, "g");
         if ($(this).attr("href").match(/\/\//) && !$(this).attr("href").match(re)) {
-            // Add rel="me" to a link
-            $(this).attr("rel", "me");
+            // Prepend "me" to rel attribute of links (safe, if rel attr already exists)
+            var curRel = $(this).attr("rel");
+            $(this).attr("rel", "me " + curRel);
         }
     });
     
