01da4f0532c2839f350b3725dc8e52b50d1ddddc
Christopher Bull <3485184+ChristopherBull@users.noreply.github.com>
2022-11-28 10:12:12 +0000

    perf: Improveme addition of rel=me
    Minor improvement by moving from Regex match to Regex test.

--- assets/js/all.non-critical.js
+++ assets/js/all.non-critical.js
@@ -2,11 +2,13 @@ $(function () {
 
     // Add rel="me" to Social links, allowing social sites to verify ownership of sites
     $("ul.social-icons a[href]").each(function() {
-        // Only choose outgoing links
+        // Prepare to check current host
         var host = document.location.host;
         var re = new RegExp(host, "g");
-        if ($(this).attr("href").match(/\/\//) && !$(this).attr("href").match(re)) {
-            // Prepend "me" to rel attribute of links (safe, if rel attr already exists)
+        // Only choose external links
+        // Performs fast check and skip if link starts with "/" (relative link), then checks if an absolute link goes to an external site.
+        if (!this.href.startsWith("/") && !re.test(this.href)) {
+            // Prepend "me" to rel attribute of links (NB: this is safe, if rel attr already exists)
             var curRel = $(this).attr("rel");
             $(this).attr("rel", "me " + curRel);
         }
