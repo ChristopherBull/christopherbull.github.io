58e5689ded6343bf4418a219cfec5727a8a5f6b8
Christopher Bull <caffeinatedandroid@gmail.com>
2025-01-23 17:09:10 +0000

    perf: minor custom JS optimisation

--- assets/js/all.non-critical.js
+++ assets/js/all.non-critical.js
@@ -1,15 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Add rel="me" to Social links (especially on all page footers), allowing social sites to verify ownership of sites
-    document.querySelectorAll("ul.social-icons a[href]").forEach(function(link) {
-        // Prepare to check current host
-        const host = document.location.host;
-        const re = new RegExp(host, "g");
-        // Only choose external links
-        // Performs fast check and skip if link starts with "/" (relative link), then checks if an absolute link goes to an external site.
+document.addEventListener("DOMContentLoaded", () => {
+    // Add rel="me" to Social links (especially on all page footers),
+    // allowing social sites to verify ownership of sites
+    const host = document.location.host;
+    const re = new RegExp(host, "g");
+    document.querySelectorAll("ul.social-icons a[href]").forEach(link => {
+        // Prepend "me" to rel attribute of links (NB: this is safe, if rel attr already exists)
+        // Only choose external links and perform fast check and skip if link starts with "/" (relative link)
         if (!link.href.startsWith("/") && !re.test(link.href)) {
-            // Prepend "me" to rel attribute of links (NB: this is safe, if rel attr already exists)
-            const curRel = link.getAttribute("rel");
-            link.setAttribute("rel", "me " + curRel);
+            link.rel = `me ${link.rel || ""}`.trim();
         }
     });
 });
