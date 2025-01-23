7aad6bc31c01982809e9152944b19426b6c6f3d1
Christopher Bull <caffeinatedandroid@gmail.com>
2025-01-23 16:58:51 +0000

    refactor: convert custom JS to vanilla

--- assets/js/all.non-critical.js
+++ assets/js/all.non-critical.js
@@ -1,17 +1,15 @@
-$(function () {
-
-    // Add rel="me" to Social links, allowing social sites to verify ownership of sites
-    $("ul.social-icons a[href]").each(function() {
+document.addEventListener("DOMContentLoaded", function () {
+    // Add rel="me" to Social links (especially on all page footers), allowing social sites to verify ownership of sites
+    document.querySelectorAll("ul.social-icons a[href]").forEach(function(link) {
         // Prepare to check current host
-        var host = document.location.host;
-        var re = new RegExp(host, "g");
+        const host = document.location.host;
+        const re = new RegExp(host, "g");
         // Only choose external links
         // Performs fast check and skip if link starts with "/" (relative link), then checks if an absolute link goes to an external site.
-        if (!this.href.startsWith("/") && !re.test(this.href)) {
+        if (!link.href.startsWith("/") && !re.test(link.href)) {
             // Prepend "me" to rel attribute of links (NB: this is safe, if rel attr already exists)
-            var curRel = $(this).attr("rel");
-            $(this).attr("rel", "me " + curRel);
+            const curRel = link.getAttribute("rel");
+            link.setAttribute("rel", "me " + curRel);
         }
     });
-    
 });
